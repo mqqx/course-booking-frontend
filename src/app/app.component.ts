597afc0edd2dc4ff7e3754123b5cbf0cf8791527
mqqx <course-booking-frontend@hmmr.dev>
2020-04-11 18:31:21 +0200

    change selector prefix to own prefix hmmr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hmmr-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-booking-frontend';
+  title = 'Course Booking';
 }
